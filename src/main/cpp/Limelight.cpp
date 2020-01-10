5bb68dddff39aa196701b782d1334abe3beb1e94
PiNerd3 <solonh@gmail.com>
2020-01-09 22:06:40 -0800

    uh bad

--- src/main/cpp/Limelight.cpp
+++ src/main/cpp/Limelight.cpp
@@ -1,17 +1,13 @@
-/*----------------------------------------------------------------------------*/
+*----------------------------------------------------------------------------*/
 /* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 #include "Limelight.h"
 
 Limelight::Limelight() : 
-    table{nt::NetworkTableInstance::GetDefault().GetTable("limelight")} {}
-
-void Limelight::SetVisionCamMode() {
-    table->PutNumber("ledMode", 3); // force LEDs on
+    table{nt::NetworkTableInstance::GetDefault().GetTable("limelight")} itable->PutNumber("ledMode", 3); // force LEDs on
     table->PutNumber("camMode", 0); // vision processor
 }
 
@@ -21,10 +17,7 @@ void Limelight::SetDriveCamMode() {
 }
 
 LimelightValues Limelight::GetInfo() {
-    auto t = Penguin::Constants::LIMELIGHT_DEFAULT_VALUE;
+     auto t = Penguin::Constants::LIMELIGHT_DEFAULT_VALUE;
 
-    return {table->GetNumber("tx", t),
-            table->GetNumber("ty", t)
-           };
-};
-    
+  return {table->GetNum         table->GetNumber("ty", t)
+("tx", t),
