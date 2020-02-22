e8ed8ad899de965ad6d5afbbe180ecf686171d22
PiNerd3 <solonh@gmail.com>
2020-02-22 15:09:23 -0800

    tidy comments

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 #include <units/units.h>
 
 #include "Robot.h"
@@ -23,34 +16,14 @@ void Robot::RobotInit() {
 
 }
 
-/**
- * This function is called every robot packet, no matter the mode. Use
- * this for items like diagnostics that you want ran during disabled,
- * autonomous, teleoperated and test.
- *
- * <p> This runs after the mode specific periodic functions, but before
- * LiveWindow and SmartDashboard integrated updating.
- */
+ /* Runs every packet. Runs after the mode specific periodic functions, but before LiveWindow and SmartDashboard integrated updating. */
 void Robot::RobotPeriodic() {
   m_drivetrain.PutDiagnostics();
   m_drivetrain.Update();
 }
 
-/**
- * This autonomous (along with the chooser code above) shows how to select
- * between different autonomous modes using the dashboard. The sendable chooser
- * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
- * remove all of the chooser code and uncomment the GetString line to get the
- * auto name from the text box below the Gyro.
- *
- * You can add additional auto modes by adding additional comparisons to the
- * if-else structure below with additional strings. If using the SendableChooser
- * make sure to add them to the chooser code above as well.
- */
 void Robot::AutonomousInit() {
   m_autoSelected = m_chooser.GetSelected();
-  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
-  //     kAutoNameDefault);
   std::cout << "Auto selected: " << m_autoSelected << std::endl;
 
   if (m_autoSelected == kAutoNameCustom) {
@@ -70,7 +43,6 @@ void Robot::AutonomousInit() {
     // Pass the config
     trajectoryConfig      
   );
-
 }
 
 void Robot::AutonomousPeriodic() {
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 #pragma once
 
 #include <string>
@@ -46,10 +39,11 @@ class Robot : public frc::TimedRobot {
   const std::string kAutoNameCustom = "My Auto";
   std::string m_autoSelected;
 
+
   Limelight limelight;
 
-  
   frc::Timer m_timer; 
+
   frc::Joystick m_leftJoystick{0};
   frc::Joystick m_rightJoystick{1};
   frc::Joystick m_gamerJoystick{2};
