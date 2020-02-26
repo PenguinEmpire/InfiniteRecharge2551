1890390593e37ce065f04526cc6889ecde28d815
PiNerd3 <solonh@gmail.com>
2020-02-25 18:54:50 -0800

    add driver station maybe

--- src/main/include/WheelOfFortune.h
+++ src/main/include/WheelOfFortune.h
@@ -9,6 +9,7 @@
 
 #include <frc/util/color.h>
 #include "frc/smartdashboard/SmartDashboard.h"
+#include "frc/DriverStation.h"
 
 #include "rev/ColorSensorV3.h"
 #include "rev/ColorMatch.h"
@@ -22,29 +23,20 @@ class WheelOfFortune {
  private:
   static constexpr auto I2C_PORT = frc::I2C::Port::kOnboard;
 
-  /**
-   * A Rev Color Sensor V3 object is constructed with an I2C port as a 
-   * parameter. The device will be automatically initialized with default 
-   * parameters.
-   */
+  /** A Rev Color Sensor V3 object is constructed with an I2C port as a parameter. The device will be automatically initialized with default parameters. */
   rev::ColorSensorV3 m_colorSensor{I2C_PORT};
 
-  /**
-   * A Rev Color Match object is used to register and detect known colors. This can 
-   * be calibrated ahead of time or during operation.
-   * 
-   * This object uses a simple euclidian distance to estimate the closest match
-   * with given confidence range.
+  /** A Rev Color Match object is used to register and detect known colors. This can be calibrated  head of time or during operation.
+   * This object uses a simple euclidian distance to estimate the closest match with given confidence range. 
    */
   rev::ColorMatch m_colorMatcher;
 
-  /**
-   * Note: Any example colors should be calibrated as the user needs, these
-   * are here as a basic example.
-   */
+  // Colors as given in the rev example
   static constexpr frc::Color kBlueTarget = frc::Color(0.143, 0.427, 0.429);
   static constexpr frc::Color kGreenTarget = frc::Color(0.197, 0.561, 0.240);
   static constexpr frc::Color kRedTarget = frc::Color(0.561, 0.232, 0.114);
   static constexpr frc::Color kYellowTarget = frc::Color(0.361, 0.524, 0.113);
+
+  // auto m_DS = frc::DriverStation::GetInstance();
 
 };
