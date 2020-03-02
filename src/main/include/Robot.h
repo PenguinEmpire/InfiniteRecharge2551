0e2d8ad5d59ebaccc69d1280dc0028537e01fd16
PiNerd3 <solonh@gmail.com>
2020-03-02 00:33:49 -0800

    reorganize header

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -29,6 +29,8 @@
 
 class Robot : public frc::TimedRobot {
  public:
+
+  // Default functions
   void RobotInit() override;
   void RobotPeriodic() override;
   void AutonomousInit() override;
@@ -37,39 +39,45 @@ class Robot : public frc::TimedRobot {
   void TeleopPeriodic() override;
   void TestPeriodic() override;
 
+  // Autonomous selection
   frc::SendableChooser<std::string> m_chooser;
   const std::string kAutoNameDefault = "Default";
   const std::string trajectoryAutoName = "trajectoryAutoTest";
   const std::string limelightAutoName = "limelightAutoTest";
   std::string m_autoSelected;
 
-
-  Limelight limelight;
-
+  // Global state
   frc2::Timer m_timer;
   units::second_t m_currentTime = units::second_t(0);
 
-  frc::Joystick m_leftJoystick{0};
-  frc::Joystick m_rightJoystick{1};
-  frc::Joystick m_gamerJoystick{2};
-  
+  // Subsystems
+  Limelight limelight;
   SwerveDrive m_drivetrain;
 
+  // Autonomous classes
+  LimelightAutonomous limelightAuto{&limelight};
+
+  // Other functions
   void ProcessJoysticks();
   void Drive();
   void ConfigESCs();
 
-  std::shared_ptr<WPI_TalonSRX> elevator = std::make_shared<WPI_TalonSRX>(PenguinConstants::CAN::ELEVATOR_MASTER);
-  std::shared_ptr<WPI_VictorSPX> elevatorHelper = std::make_shared<WPI_VictorSPX>(PenguinConstants::CAN::ELEVATOR_SLAVE);
-  std::shared_ptr<WPI_TalonSRX> intake = std::make_shared<WPI_TalonSRX>(PenguinConstants::CAN::INTAKE);
-  std::shared_ptr<WPI_TalonSRX> belt = std::make_shared<WPI_TalonSRX>(PenguinConstants::CAN::BELT);
-  std::shared_ptr<WPI_TalonSRX> aimer = std::make_shared<WPI_TalonSRX>(PenguinConstants::CAN::AIMER);
-  std::shared_ptr<WPI_TalonSRX> shooter = std::make_shared<WPI_TalonSRX>(PenguinConstants::CAN::SHOOTER);
-  std::shared_ptr<WPI_TalonSRX> centerer = std::make_shared<WPI_TalonSRX>(PenguinConstants::CAN::CENTERER);
+  // Global devices
+    // Joysticks
+  frc::Joystick m_leftJoystick{0};
+  frc::Joystick m_rightJoystick{1};
+  frc::Joystick m_gamerJoystick{2};
+  
+    // Other motors
+  std::shared_ptr<WPI_TalonSRX> m_elevator = std::make_shared<WPI_TalonSRX>(PenguinConstants::CAN::ELEVATOR_MASTER);
+  std::shared_ptr<WPI_VictorSPX> m_elevatorHelper = std::make_shared<WPI_VictorSPX>(PenguinConstants::CAN::ELEVATOR_SLAVE);
+  std::shared_ptr<WPI_TalonSRX> m_intake = std::make_shared<WPI_TalonSRX>(PenguinConstants::CAN::INTAKE);
+  std::shared_ptr<WPI_TalonSRX> m_belt = std::make_shared<WPI_TalonSRX>(PenguinConstants::CAN::BELT);
+  std::shared_ptr<WPI_TalonSRX> m_aimer = std::make_shared<WPI_TalonSRX>(PenguinConstants::CAN::AIMER);
+  std::shared_ptr<WPI_TalonSRX> m_shooter = std::make_shared<WPI_TalonSRX>(PenguinConstants::CAN::SHOOTER);
+  std::shared_ptr<WPI_TalonSRX> m_centerer = std::make_shared<WPI_TalonSRX>(PenguinConstants::CAN::CENTERER);
 
-  LimelightAutonomous limelightAuto{&limelight};
-
-
-  std::shared_ptr<frc::Encoder> shooterEncoder = std::make_shared<frc::Encoder>(PenguinConstants::DIO::SHOOTER_ENCODER_A, PenguinConstants::DIO::SHOOTER_ENCODER_B);
-  std::shared_ptr<frc::Encoder> elevatorEncoder = std::make_shared<frc::Encoder>(PenguinConstants::DIO::ELEVATOR_ENCODER_A, PenguinConstants::DIO::ELEVATOR_ENCODER_B);
+    // Encoders
+  std::shared_ptr<frc::Encoder> m_shooterEncoder = std::make_shared<frc::Encoder>(PenguinConstants::DIO::SHOOTER_ENCODER_A, PenguinConstants::DIO::SHOOTER_ENCODER_B);
+  std::shared_ptr<frc::Encoder> m_elevatorEncoder = std::make_shared<frc::Encoder>(PenguinConstants::DIO::ELEVATOR_ENCODER_A, PenguinConstants::DIO::ELEVATOR_ENCODER_B);
 };
